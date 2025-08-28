736df6e812664ab354b24f781582630b2d420156
swiftuihome <zelda.foxmail@gmail.com>
2025-08-28 23:56:22 +0800

    init

---
+++ CaptureSample/CaptureSampleApp.swift
@@ -0,0 +1,18 @@
+/*
+See the LICENSE.txt file for this sample’s licensing information.
+
+Abstract:
+The entry point into this app.
+*/
+import SwiftUI
+
+@main
+struct CaptureSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .frame(minWidth: 960, minHeight: 724)
+                .background(.black)
+        }
+    }
+}
